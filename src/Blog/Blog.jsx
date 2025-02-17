8b2c4f63ad041aba17f85f007713dc56eb37b50e
Tauhid hossen <tauhidhossen04@gmail.com>
2025-02-17 21:26:20 +0600

    Start pagina section Update

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -1,131 +1,160 @@
-
-
 const Blog = () => {
-    return (
-        <div>
-             <div className=" grid grid-cols-1 lg:grid-cols-3 ml-10 mt-20 mr-10 mb-20 gap-6 md:px-28">
-                <div className="">
-                            <img src="/article/Article.png" alt="" />
-                            <div className="">
-                            <div className="flex gap-2 mb-4 mt-2">
-                                <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
-                                    Website Design
-                                </button>
-                                <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
-                                    App Dev
-                                </button>
-                                </div>
-                            </div>
-
-                            <h1 className="mb-10 text-xl font-bold mt-6 text-left">“5 manieren om uw website te laten opvallen”</h1>
-                            <p className="text-slate-400 text-left text-xs">Een goed ontworpen website is meer dan een visitekaartje</p>
-                            <p className="text-slate-400 text-left text-xs">Leer hoe u met design, snelheid en content uw conversies..</p>
-
-                            <div className="flex justify-start mt-5 "><button className="btn btn-sm btn-info text-slate-200">Lees meer</button></div>
-                            </div>
-
-                        
-
-                            <div className="">
-                            <img src="/article/Link → blog-6.jpg.png" alt="" />
-                            <div className="">
-                            <div className="flex gap-2 mb-4 mt-2">
-                                <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
-                                    Website Design
-                                </button>
-                                <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
-                                    App Dev
-                                </button>
-                                </div>
-                            </div>
-
-                            <h1 className="mb-10 text-xl font-bold mt-6 text-left">“5 manieren om uw website te laten opvallen”</h1>
-                            <p className="text-slate-400 text-left text-xs">Een goed ontworpen website is meer dan een visitekaartje</p>
-                            <p className="text-slate-400 text-left text-xs">Leer hoe u met design, snelheid en content uw conversies..</p>
-
-                            <div className="flex justify-start mt-5"><button className="btn btn-sm text-slate-200 btn-info">Lees meer</button></div>
-                            </div>
-
-
-
-                            <div className="">
-                            <img src="/article/Link → blog-6.jpg (1).png" alt="" />
-                            <div className="">
-                            <div className="flex gap-2 ml-7 mb-4 mt-2">
-                                <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
-                                    Website Design
-                                </button>
-                                <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
-                                    App Dev
-                                </button>
-                                </div>
-                            </div>
-
-                            <h1 className="mb-10 text-xl font-bold mt-6 text-left">“5 manieren om uw website te laten opvallen”</h1>
-                            <p className="text-slate-400 text-left text-xs">Een goed ontworpen website is meer dan een visitekaartje</p>
-                            <p className="text-slate-400 text-left text-xs">Leer hoe u met design, snelheid en content uw conversies..</p>
-
-                            <div className="flex justify-start  mt-5"><button className="btn btn-sm text-slate-200  btn-info">Lees meer</button></div>
-                            
-                            </div>
-
-
-
-
-                            <div className="">
-                            <img src="/article/Article (1).png" alt="" />
-                            <div className="">
-                            <div className="flex gap-2 mb-4 mt-2">
-                                <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
-                                    Website Design
-                                </button>
-                                <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
-                                    App Dev
-                                </button>
-                                </div>
-                            </div>
-
-                            <h1 className="mb-10 text-xl font-bold mt-6 text-left">“5 manieren om uw website te laten opvallen”</h1>
-                            <p className="text-slate-400 text-left text-xs">Een goed ontworpen website is meer dan een visitekaartje</p>
-                            <p className="text-slate-400 text-left text-xs">Leer hoe u met design, snelheid en content uw conversies..</p>
-
-                            <div className="flex justify-start mt-5"><button className="btn btn-sm text-slate-200  btn-info">Lees meer</button></div>
-                            
-                            </div>
-
-
-
-
-                            <div className="">
-                            <img src="/article/Article.png" alt="" />
-                            
-                            <div className="">
-                            <div className="flex gap-2 mb-4 mt-2">
-                                <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
-                                    Website Design
-                                </button>
-                                <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
-                                    App Dev
-                                </button>
-                                </div>
-                            </div>
-
-                            <h1 className="mb-10 text-xl font-bold mt-6 text-left">“5 manieren om uw website te laten opvallen”</h1>
-                            <p className="text-slate-400 text-left text-xs">Een goed ontworpen website is meer dan een visitekaartje</p>
-                            <p className="text-slate-400 text-left text-xs">Leer hoe u met design, snelheid en content uw conversies..</p>
-
-                            <div className="flex justify-start mt-5"><button className="btn btn-sm text-slate-200  btn-info">Lees meer</button></div>
-                            
-                            </div>
-
-                            
-                            
+  return (
+    <div>
+      <div className=" grid grid-cols-1 lg:grid-cols-3 ml-10 mt-20 mr-10 mb-20 gap-6 md:px-28">
+        <div className="">
+          <img src="/article/Article.png" alt="" />
+          <div className="">
+            <div className="flex gap-2 mb-4 mt-2">
+              <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
+                Website Design
+              </button>
+              <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
+                App Dev
+              </button>
             </div>
+          </div>
 
+          <h1 className="mb-10 text-xl font-bold mt-6 text-left">
+            “5 manieren om uw website te laten opvallen”
+          </h1>
+          <p className="text-slate-400 text-left text-xs">
+            Een goed ontworpen website is meer dan een visitekaartje
+          </p>
+          <p className="text-slate-400 text-left text-xs">
+            Leer hoe u met design, snelheid en content uw conversies..
+          </p>
 
-
+          <div className="flex justify-start mt-5 ">
+            <button className="btn btn-sm btn-info text-slate-200">
+              Lees meer
+            </button>
+          </div>
         </div>
-    );
+
+        <div className="">
+          <img src="/article/Link → blog-6.jpg.png" alt="" />
+          <div className="">
+            <div className="flex gap-2 mb-4 mt-2">
+              <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
+                Website Design
+              </button>
+              <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
+                App Dev
+              </button>
+            </div>
+          </div>
+
+          <h1 className="mb-10 text-xl font-bold mt-6 text-left">
+            “5 manieren om uw website te laten opvallen”
+          </h1>
+          <p className="text-slate-400 text-left text-xs">
+            Een goed ontworpen website is meer dan een visitekaartje
+          </p>
+          <p className="text-slate-400 text-left text-xs">
+            Leer hoe u met design, snelheid en content uw conversies..
+          </p>
+
+          <div className="flex justify-start mt-5">
+            <button className="btn btn-sm text-slate-200 btn-info">
+              Lees meer
+            </button>
+          </div>
+        </div>
+
+        <div className="">
+          <img src="/article/Link → blog-6.jpg (1).png" alt="" />
+          <div className="">
+            <div className="flex gap-2 ml-7 mb-4 mt-2">
+              <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
+                Website Design
+              </button>
+              <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
+                App Dev
+              </button>
+            </div>
+          </div>
+
+          <h1 className="mb-10 text-xl font-bold mt-6 text-left">
+            “5 manieren om uw website te laten opvallen”
+          </h1>
+          <p className="text-slate-400 text-left text-xs">
+            Een goed ontworpen website is meer dan een visitekaartje
+          </p>
+          <p className="text-slate-400 text-left text-xs">
+            Leer hoe u met design, snelheid en content uw conversies..
+          </p>
+
+          <div className="flex justify-start  mt-5">
+            <button className="btn btn-sm text-slate-200  btn-info">
+              Lees meer
+            </button>
+          </div>
+        </div>
+
+        <div className="">
+          <img src="/article/Article (1).png" alt="" />
+          <div className="">
+            <div className="flex gap-2 mb-4 mt-2">
+              <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
+                Website Design
+              </button>
+              <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
+                App Dev
+              </button>
+            </div>
+          </div>
+
+          <h1 className="mb-10 text-xl font-bold mt-6 text-left">
+            “5 manieren om uw website te laten opvallen”
+          </h1>
+          <p className="text-slate-400 text-left text-xs">
+            Een goed ontworpen website is meer dan een visitekaartje
+          </p>
+          <p className="text-slate-400 text-left text-xs">
+            Leer hoe u met design, snelheid en content uw conversies..
+          </p>
+
+          <div className="flex justify-start mt-5">
+            <button className="btn btn-sm text-slate-200  btn-info">
+              Lees meer
+            </button>
+          </div>
+        </div>
+
+        <div className="">
+          <img src="/article/Article.png" alt="" />
+
+          <div className="">
+            <div className="flex gap-2 mb-4 mt-2">
+              <button className="bg-blue-500 text-white text-sm px-4 py-1 rounded-full hover:bg-blue-600 transition duration-300">
+                Website Design
+              </button>
+              <button className="bg-green-500 text-white text-sm px-4 py-1 rounded-full hover:bg-green-600 transition duration-300">
+                App Dev
+              </button>
+            </div>
+          </div>
+
+          <h1 className="mb-10 text-xl font-bold mt-6 text-left">
+            “5 manieren om uw website te laten opvallen”
+          </h1>
+          <p className="text-slate-400 text-left text-xs">
+            Een goed ontworpen website is meer dan een visitekaartje
+          </p>
+          <p className="text-slate-400 text-left text-xs">
+            Leer hoe u met design, snelheid en content uw conversies..
+          </p>
+
+          <div className="flex justify-start mt-5">
+            <button className="btn btn-sm text-slate-200  btn-info">
+              Lees meer
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/Startpagina/startpagina.jsx
+++ src/Startpagina/startpagina.jsx
@@ -8,60 +8,135 @@ const Startpagina = () => {
       {/* Left Section */}
       <div className="md:w-1/2 flex flex-col items-start gap-4">
         <img src={img} alt="Illustration" className="w-full h- rounded-lg" />
-        <h2 className="text-2xl font-bold text-gray-800">
+        <h2 className="text-3xl font-bold text-gray-800">
           Kies een plan en begin meteen
         </h2>
         <p className="text-gray-600 leading-relaxed">
-          Het afdrukken van organisatiemotto's. Hoewel het internet de controle
-          over veel aspecten van afdrukken kan overnemen, zijn sommige dingen
-          nog steeds gewenst op papier.
+          <span className="text-xl font-bold text-[#253238]">
+            Kies een plan en start direct:
+          </span>{" "}
+          Selecteer een van onze plannen, betaal een aanbetaling en ontvang
+          direct een offerte in je mailbox. Twijfel je nog en speelt de prijs
+          een grote rol? Vraag dan vrijblijvend onze prijslijst aan.
+        </p>
+        <p className="text-gray-600 leading-relaxed">
+          De kosten zijn afhankelijk van het doel dat je wilt bereiken. Een
+          lokale bakkerij die meer verkopen in het dorp wil realiseren,
+          verschilt bijvoorbeeld van een muziekconcert dat heel West-Europa wil
+          bereiken.
+        </p>
+        <p className="text-gray-600 leading-relaxed">
+          De opgegeven prijzen zijn onze servicekosten. Kies je voor een
+          agressieve marketingcampagne? Dan worden extra mediakosten direct
+          doorgerekend, uiteraard altijd in goed overleg.
+        </p>
+        <p className="text-[#FF4B32] leading-relaxed">
+          ⚠ Let op: Alle vermelde prijzen zijn exclusief BTW.
+        </p>
+        <p className="text-gray-600 leading-relaxed">
+          <span className="text-xl font-bold text-[#253238]">
+            Korting voor directe aanbetalingen:
+          </span>{" "}
+          Voor klanten die direct een aanbetaling doen, bieden wij een korting
+          van 20% op onze diensten. Zo profiteer je direct van een voordeliger
+          tarief!
         </p>
       </div>
 
       {/* Right Section (Cards) */}
       <div className="md:w-1/2 grid gap-6">
         {/* First Card */}
-        <div className="p-6 bg-white shadow-lg rounded-xl border border-gray-200">
+        <div className="p-6 border  bg-white shadow-lg rounded-xl  border-gray-200">
           <div className="flex justify-between items-center mb-4">
             <div className="flex items-center gap-2">
               <img src={icon} alt="Icon" className="w-4 h-4" />
-              <h2 className="text-lg font-semibold text-gray-800">Starter</h2>
+              <h2 className="text-lg font-semibold text-gray-800">Basispakket</h2>
             </div>
-            <h2 className="text-lg font-bold text-gray-800">$37 <span className="text-[#7E7E7E] text-sm">/ month</span></h2>
+            <h2 className="text-lg font-bold text-gray-800">
+            €<span className="text-[#7E7E7E] text-sm">/ maand</span>
+            </h2>
           </div>
-          <p className="text-gray-600 ">
-            Duis vestibulum, massa sit amet bibendum ultrices, mauris lacus
-            varius nunc, dapibus condimentum.
+          <p  className="md:-mb-36">
+            <ul className="list-disc pl-5">
+              <li>Opzetten Website en Google SEO.</li>
+              <li>Nulmeting (Eigen -website).</li>
+              <li>Wekelijkse Consultancy (offline Verkoop en marketing).</li>
+            
+            </ul>
           </p>
         </div>
 
         {/* Second Card */}
-        <div className="p-6 bg-white shadow-lg rounded-xl border border-gray-200">
+        <div className="p-6 border  bg-white shadow-lg rounded-xl  border-gray-200">
           <div className="flex justify-between items-center mb-4">
             <div className="flex items-center gap-2">
               <img src={icon1} alt="Icon" className="w-4 h-4" />
-              <h2 className="text-lg font-semibold text-gray-800">Professional</h2>
+              <h2 className="text-lg font-semibold text-gray-800">Standaard SEO -pakket</h2>
             </div>
-            <h2 className="text-lg font-bold text-gray-800">$67<span className="text-[#7E7E7E] text-sm">/ month</span></h2>
+            <h2 className="text-lg font-bold text-gray-800">
+            €€<span className="text-[#7E7E7E] text-sm">/ maand</span>
+            </h2>
           </div>
-          <p className="text-gray-600">
-            Duis vestibulum, massa sit amet bibendum ultrices, mauris lacus
-            varius nunc, dapibus condimentum.
+          <p  className="md:-mb-36">
+            <ul className="list-disc pl-5">
+              <li>Grondige Websiteanalse.
+              </li>
+              <li>Marktonderzoek naar concurrenten.
+              </li>
+              <li>Verbeterde SEO-proof website.
+              </li>
+              <li>Inusief alles uit het de basePakket.</li>
+            </ul>
           </p>
         </div>
 
         {/* Third Card */}
-        <div className="p-6 bg-white shadow-lg rounded-xl border border-gray-200">
+        <div className="p-6 border  bg-white shadow-lg rounded-xl  border-gray-200">
           <div className="flex justify-between items-center mb-4">
             <div className="flex items-center gap-2">
               <img src={icon1} alt="Icon" className="w-4 h-4" />
-              <h2 className="text-lg font-semibold text-gray-800">Enterprise</h2>
+              <h2 className="text-lg font-semibold text-gray-800">Regelmatig online pakket</h2>
             </div>
-            <h2 className="text-lg font-bold text-gray-800">$97<span className="text-[#7E7E7E] text-sm">/ month</span></h2>
+            <h2 className="text-lg font-bold text-gray-800">
+            €€€<span className="text-[#7E7E7E] text-sm">/ maand</span>
+            </h2>
           </div>
-          <p className="text-gray-600">
-            Duis vestibulum, massa sit amet bibendum ultrices, mauris lacus
-            varius nunc, dapibus condimentum.
+          <p  className="md:-mb-36">
+            <ul className="list-disc pl-5">
+              <li>SEO en Sociale Media Combinatie.
+              </li>
+              <li>Wekelijkse Post.
+              </li>
+              <li>Maandelijkse e-mailcampagne.
+              </li>
+              <li>Opname van Operationele Vlogs.
+              </li>
+              <li>Bevat alles Uit het reguliere pakket
+              </li>
+            </ul>
+          </p>
+        </div>
+        {/* Fourth Card */}
+        <div className="p-6 border  bg-white shadow-lg rounded-xl  border-gray-200">
+          <div className="flex justify-between items-center mb-4">
+            <div className="flex items-center gap-2">
+              <img src={icon1} alt="Icon" className="w-4 h-4" />
+              <h2 className="text-lg font-semibold text-gray-800">Premium pakket</h2>
+            </div>
+            <h2 className="text-lg font-bold text-gray-800">
+            €€€€<span className="text-[#7E7E7E] text-sm">/ maand</span>
+            </h2>
+          </div>
+          <p  className="md:-mb-36">
+            <ul className="list-disc pl-5">
+              <li>Premium SEO en -website.
+              </li>
+              <li>Creëren van Reguliere -content (plus Beheer).
+              </li>
+              <li>Professionele, authentieke content.
+              </li>
+              <li>Inclusief Alles Uit Het Regulier Online Pakket.</li>
+            </ul>
           </p>
         </div>
       </div>
