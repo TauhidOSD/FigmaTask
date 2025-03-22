bc7f8d2f987811f111d7ba1e58e79a44a5e85e50
shakibh10~ <billalsikdar03@gmail.com~>
2025-03-22 08:48:59 +0600

    More update on card

--- src/Finguide/Finguide.jsx
+++ src/Finguide/Finguide.jsx
@@ -26,35 +26,35 @@ const Finguide = () => {
   
             {/* Right Details */}
             <div className="space-y-2">
-              <h1 className=" text-xl lg:text-lg text-sky-400 font-bold">
+              <h1 className=" text-[32px] text-[rgba(70,138,255,1)] font-plus-jakarta font-bold">
               FINGUIDE: Slimme Boekhouding met een Sterke Identiteit
               </h1>
   
               {/* Details List */}
               <div className="space-y-3 border-b border-slate-200 py-2">
                 <div className="flex justify-between items-center px-2">
-                  <p className="text-sm text-slate-500">Category:</p>
-                  <p className="text-sm text-slate-500 font-medium">UI Design</p>
+                  <p className="text-[18px] font-medium font-plus-jakarta text-[rgba(38,50,56,1)]">Category:</p>
+                  <p className="text-[18px] font-plus-jakarta font-normal text-[rgba(73,73,73,1)]">UI Design</p>
                 </div>
                 
                 <div className="space-y-3 border-t border-b border-slate-200 py-3">
                 <div className="flex justify-between items-center px-2">
-                  <p className="text-sm text-slate-500">Tools:</p>
-                  <p className="text-sm text-slate-500 font-medium">Figma</p>
+                  <p className="text-[18px] font-medium font-plus-jakarta text-[rgba(38,50,56,1)]">Tools:</p>
+                  <p className="text-[18px] font-plus-jakarta font-normal text-[rgba(73,73,73,1)]">Figma</p>
                 </div>
               
                 </div>
                 <div className="">
                 <div className="flex justify-between items-center px-2">
-                  <p className="text-sm text-slate-500">Project date:</p>
-                  <p className="text-sm text-slate-500 font-medium">February, 2020</p>
+                  <p className="text-[18px] font-medium font-plus-jakarta text-[rgba(38,50,56,1)]">Project date:</p>
+                  <p className="text-[18px] font-plus-jakarta font-normal text-[rgba(73,73,73,1)]">February, 2020</p>
                 </div>
              
                 </div>
                 <div className=" border-t border-slate-200 pt-3">
                 <div className="flex justify-between items-center px-2">
-                  <p className="text-sm text-slate-500">Share this:</p>
-                  <p className="text-sm text-slate-500 font-medium">
+                  <p className="text-[18px] font-medium font-plus-jakarta text-[rgba(38,50,56,1)]">Share this:</p>
+                  <p className="text-[18px] font-plus-jakarta font-normal text-[rgba(73,73,73,1)]">
                     Facebook, Twitter, Linkedin
                   </p>
                 </div>
@@ -71,7 +71,7 @@ const Finguide = () => {
   
           {/* Description */}
           <div className="mt-10 space-y-4 px-4 sm:px-6 md:px-8 lg:ml-5 lg:mr-20">
-            <p className="text-left text-sm text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
+            <p className="text-left text-[22px] text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
             In de wereld van financiële software draait het vaak om cijfers, berekeningen 
             en strakke processen. Maar hoe maak je een boekhoudapplicatie niet alleen 
             functioneel, maar ook aantrekkelijk voor ondernemers? Met FINGUIDE hebben 
@@ -96,20 +96,20 @@ const Finguide = () => {
           </div>
           <div className="space-y-4 px-4 sm:px-6 md:px-8 lg:ml-20 lg:mr-20">
             <h1 className="text-[22px] font-plus-jakarta text-[rgba(38,50,56,1)] font-semibold ">Waarom FINGUIDE?</h1>
-            <p className="text-left text-sm text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
+            <p className="text-left text-[22px] text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
             Met het groeiende aantal zzp’ers en mkb-bedrijven in Nederland is er een dringende 
             behoefte aan toegankelijke en efficiënte boekhoudsoftware. Grote corporates hebben belastingadviseurs 
             en accountantskantoren tot hun beschikking, maar de kleine ondernemer heeft vaak noch de tijd, noch
              de financiële middelen om zich hierin te verdiepen. En dat terwijl een goed inzicht in financiën cruciaal
               is voor duurzame groei.
             </p>
-            <p className="text-left text-sm text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
+            <p className="text-left text-[22px] text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
             FINGUIDE is ontwikkeld met deze visie: een intuïtieve en betaalbare oplossing die ondernemers helpt bij het automatiseren van 
             hun administratie, hen voorziet van slimme adviezen en hen helpt om sterke financiële gewoontes op te bouwen. Dit is geen 
             boekhoudprogramma dat puur op functionaliteit leunt; het is een totaalconcept waarin branding, gebruiksvriendelijkheid en 
             strategische begeleiding samenkomen.
             </p>
-            <p className="text-left text-sm text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
+            <p className="text-left text-[22px] text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
             De website fungeert niet alleen als een portfolio, waarin eerdere werken en projecten schitterend worden weergegeven,
              maar ook als een webshop waar klanten direct henna-producten en 
              gepersonaliseerde diensten kunnen bestellen. Dit maakt het voor 
@@ -131,7 +131,7 @@ const Finguide = () => {
           </div>
           <div className="space-y-4 px-4 sm:px-6 md:px-8 lg:ml-20 lg:mr-20">
             <h1 className="text-[22px] font-plus-jakarta text-[rgba(38,50,56,1)] font-semibold">Branding: Vertrouwen, Simpelheid en Toegankelijkheid</h1>
-            <p className="text-left text-sm text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
+            <p className="text-left text-[22px] text-[rgba(121,121,121,1)] font-plus-jakarta font-normal leading-relaxed">
             Boekhouding is voor veel ondernemers een verplichting, geen passie. Daarom moest de branding van FINGUIDE 
             anders zijn dan de traditionele, vaak droge uitstraling van boekhoudsoftware. Younitech heeft een 
             merk gecreëerd dat vertrouwen uitstraalt, maar tegelijkertijd vriendelijk en toegankelijk 
@@ -140,11 +140,11 @@ const Finguide = () => {
 
 
             <ul className="list-disc list-inside text-left text-sm font-sans  leading-relaxed" >
-                <li> <span className="text-[rgba(121,121,121,1)]">Kleurgebruik: FINGUIDE maakt gebruik van een eenvoudig en strak kleurenpalet dat draait om blauw- en grijstonen, gecombineerd met subtiele accentkleuren. Blauw staat voor betrouwbaarheid en stabiliteit, terwijl de zachte grijstinten een moderne en professionele uitstraling geven.</span></li>
+                <li> <span className="text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)]">Kleurgebruik: FINGUIDE maakt gebruik van een eenvoudig en strak kleurenpalet dat draait om blauw- en grijstonen, gecombineerd met subtiele accentkleuren. Blauw staat voor betrouwbaarheid en stabiliteit, terwijl de zachte grijstinten een moderne en professionele uitstraling geven.</span></li>
 
-                 <li> <span className="text-[rgba(121,121,121,1)]">Gebruiksvriendelijke interface: De app ziet er niet uit als een traditioneel, complex boekhoudprogramma. Heldere visuals, duidelijke instructies en een gestroomlijnd dashboard maken het gebruik laagdrempelig.</span></li>
+                 <li> <span className="text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)]">Gebruiksvriendelijke interface: De app ziet er niet uit als een traditioneel, complex boekhoudprogramma. Heldere visuals, duidelijke instructies en een gestroomlijnd dashboard maken het gebruik laagdrempelig.</span></li>
 
-                 <li> <span className="text-[rgba(121,121,121,1)]">Educatieve content: FINGUIDE biedt meer dan alleen software; het is een bron van kennis. Door middel van tutorials en casestudy’s met echte ondernemers wordt boekhouden begrijpelijk en toepasbaar gemaakt.</span></li>
+                 <li> <span className="text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)]">Educatieve content: FINGUIDE biedt meer dan alleen software; het is een bron van kennis. Door middel van tutorials en casestudy’s met echte ondernemers wordt boekhouden begrijpelijk en toepasbaar gemaakt.</span></li>
 
             </ul>
 
@@ -152,17 +152,17 @@ const Finguide = () => {
 
           <div className="space-y-4 px-4 sm:px-6 md:px-8 lg:ml-20 lg:mr-20">
             <h1 className="text-[22px] font-plus-jakarta text-[rgba(38,50,56,1)] font-semibold">Functionaliteiten die Ondernemers Echt Helpen</h1>
-            <p className="text-left text-sm text-slate-500 leading-relaxed">
+            <p className="text-left text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)] leading-relaxed">
             
                 Los van de branding biedt FINGUIDE alles wat een moderne ondernemer nodig heeft:
             </p>
 
 
             <ul className="list-disc list-inside text-left text-sm font-sans  leading-relaxed" >
-                <li> <span className="text-[rgba(121,121,121,1)]">Automatiseren van administratieve taken: Facturen uploaden, bonnetjes inscannen en direct verwerken.</span></li>
-                 <li> <span className="text-[rgba(121,121,121,1)]">Live inzicht in financiële status: Altijd een actueel overzicht van winst- en verliesrekeningen.</span></li>
-                 <li> <span className="text-[rgba(121,121,121,1)]">Cashflowberekeningen en prognoses: Helpt ondernemers vooruit te plannen en financiële valkuilen te vermijden.</span></li>
-                 <li> <span className="text-[rgba(121,121,121,1)]">Slimme belastingadviezen: Gericht op het mkb en zzpers, zodat ook kleine bedrijven optimaal profiteren van fiscale voordelen.
+                <li> <span className="text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)]">Automatiseren van administratieve taken: Facturen uploaden, bonnetjes inscannen en direct verwerken.</span></li>
+                 <li> <span className="text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)]">Live inzicht in financiële status: Altijd een actueel overzicht van winst- en verliesrekeningen.</span></li>
+                 <li> <span className="text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)]">Cashflowberekeningen en prognoses: Helpt ondernemers vooruit te plannen en financiële valkuilen te vermijden.</span></li>
+                 <li> <span className="text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)]">Slimme belastingadviezen: Gericht op het mkb en zzpers, zodat ook kleine bedrijven optimaal profiteren van fiscale voordelen.
                  FINGUIDE is meer dan alleen een boekhoudprogramma; het is een digitale financieel adviseur die ondernemers ondersteunt bij hun groei.</span></li>
 
             </ul>
@@ -171,12 +171,12 @@ const Finguide = () => {
 
           <div className="space-y-4 px-4 sm:px-6 md:px-8 lg:ml-20 lg:mr-20">
             <h1 className="text-[22px] font-plus-jakarta text-[rgba(38,50,56,1)] font-semibold">Een Merk dat Groeit met de Ondernemer Mee</h1>
-            <p className="text-left text-sm text-slate-500 leading-relaxed">
+            <p className="text-left text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)] leading-relaxed">
             De kracht van FINGUIDE ligt in de combinatie van branding en functionaliteit. De applicatie is niet alleen een tool, maar ook een partner voor ondernemers. Het biedt niet alleen cijfers, maar helpt ook bij het maken van strategische beslissingen.
             Door in te zetten op een sterke merkidentiteit, blijft FINGUIDE niet alleen relevant in een concurrerende markt, maar weet het ook een gemeenschap van gebruikers op te bouwen die zich herkennen in de missie van het merk: financiële helderheid en succes voor iedereen, ongeacht de grootte van hun bedrijf.
             </p>
 
-            <p className="text-left text-sm text-slate-500 leading-relaxed">
+            <p className="text-left text-[22px] font-normal font-plus-jakarta text-[rgba(121,121,121,1)] leading-relaxed">
             Met een website en marketingstrategie die dezelfde kernwaarden uitstralen, wordt FINGUIDE een herkenbaar en vertrouwd merk binnen de fintech-sector. Want een applicatie mag dan nog zo goed werken, zonder de juiste branding blijft het slechts een van de velen. Bij Younitech begrijpen we dat, en met FINGUIDE zetten we een nieuw standaard neer voor fintech-applicaties.
             </p>
             
