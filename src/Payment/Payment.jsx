ad6515a937de835b62c83c99478d94842bbc95e4
Tauhid hossen <tauhidhossen04@gmail.com>
2025-03-29 21:42:37 +0600

    Post Methos added into the fetch for sent inputField value to the server side

--- src/Payment/Payment.jsx
+++ src/Payment/Payment.jsx
@@ -28,6 +28,20 @@ const Payment = () => {
     };
 
     console.log(user);
+
+    fetch('http://localhost:5000/users',{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      
+    })
+
   };
 
   return (
